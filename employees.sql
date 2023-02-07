9915e7bb5e4e4ac62273a5e9de14a16f55bdc21b
alireza alizadeh <alireza.aa9700@gmail.com>
2023-02-07 12:18:02 +0330

    create employees table,insert it value and CHECK ON IT

---
+++ employees.sql
@@ -0,0 +1,15 @@
+CREATE TABLE employees (
+	employee_id	INT(10),
+    first_name	CHAR(50),
+    last_name	CHAR(50),
+    hourly_pay	DECIMAL(5,2),
+    hire_date	DATE
+    -- CONSTRAINT chk_hourly_pay CHECK (hourly_pay <= 10.00)
+);
+
+insert INTO employees
+values(1, "alireza", "alizadeh", 25.50, current_date),
+(2, "amir", "ghorbani", 15.00, current_date),
+(3, "saleh", "shirzad", 12.50, current_date),
+(4, "reza", "zabihi", 12.50, current_date),
+(5, "mohsen", "karimi", 17.25, current_date);
