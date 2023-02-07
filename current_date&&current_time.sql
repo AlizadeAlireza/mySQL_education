e2fb888d2b400d331552281b7400c5015589d61e
alireza alizadeh <alireza.aa9700@gmail.com>
2023-02-07 08:51:33 +0330

    we get the yesterday and tommorow

--- current_date&&current_time.sql
+++ current_date&&current_time.sql
@@ -4,7 +4,17 @@ CREATE TABLE test (
     my_datetime DATETIME
 );
 
+-- this is for the current time and date
+-- now is both of time and date
 INSERT INTO test
-VALUES(CURRENT_DATE(), CURRENT_TIME(), NOW());
+VALUES(CURRENT_DATE(), CURRENT_TIME(), NOW()); 
+
+-- the current time + 1 meaning tommorow
+INSERT INTO test
+VALUES(CURRENT_DATE() + 1, NULL, NULL);
+
+-- the current time - 1 meaning yesterday
+INSERT INTO test
+VALUES(CURRENT_DATE() - 1, NULL, NULL);
 
 SELECT * FROM test;
