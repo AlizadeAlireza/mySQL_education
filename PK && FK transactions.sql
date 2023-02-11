59c5f0897c8c369aef0db3f37b145b2849e31ab5
alireza alizadeh <alireza.aa9700@gmail.com>
2023-02-11 16:05:35 +0330

    add customers table

---
+++ PK && FK transactions.sql
@@ -0,0 +1,7 @@
+-- the transactions table is exists.
+-- add customers table for create FK
+CREATE TABLE customers (
+customer_id INT PRIMARY KEY AUTO_INCREMENT,
+first_name VARCHAR(50),
+last_name VARCHAR (50)
+);
