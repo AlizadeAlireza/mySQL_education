2878c9dab6bfd804152e3feb1078cc6033867e4f
alireza alizadeh <alireza.aa9700@gmail.com>
2023-02-11 16:08:17 +0330

    insert value in customers table

--- PK && FK transactions.sql
+++ PK && FK transactions.sql
@@ -5,3 +5,9 @@ customer_id INT PRIMARY KEY AUTO_INCREMENT,
 first_name VARCHAR(50),
 last_name VARCHAR (50)
 );
+
+-- insert into customers table 
+INSERT INTO customers (first_name, last_name)
+VALUES ("Fred", "Fish"),
+	   ("Larry", "Lobster"),
+	   ("Bubble", "Bass");
